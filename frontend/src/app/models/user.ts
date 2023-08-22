efe031ee68e9c7dd14058f12a8af6133fcaad225
skmirajbn <skmirajbn@gmail.com>
2023-08-22 14:25:49 +0600

    first commit

--- frontend/src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id!: string;
-    first_name!: string;
-    last_name!: string;
-    email_id!: string;
-    contact!: string;
-    price!: string;
-    active!: boolean;
-}
--- php_api/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "hello";
-
-if($_POST("name")){
-    echo "hello "+ $_POST("name");
-}
-?>
